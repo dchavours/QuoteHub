28d513e3517064314c7aa34fab16489ebf65dddc
Dennis Chavours <dchavours@gmail.com>
2019-08-25 17:39:55 -0500

    added node-scss

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux';
 
 import Routes from './routes/Routes';
 import store from './redux/store';
-import './App.css';
+import './App.scss';
 
 const App = () => {
   return (
