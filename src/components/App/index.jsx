5c80902b88fbe7f7a1158b9603aa59df404d6134
Dennis Chavours <dchavours@gmail.com>
2019-09-02 23:10:11 -0500

    commit

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import ModalExample from '../../components/ModalExample';
-// import { Button } from 'chramework';
 import  Button  from '../Button/Button';
 
 
