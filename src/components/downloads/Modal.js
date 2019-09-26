54f49cfffe6b56800398906212049813915acf95
Dennis Chavours <dchavours@gmail.com>
2019-09-26 14:27:08 -0500

    working on it

--- src/components/downloads/Modal.js
+++ src/components/downloads/Modal.js
@@ -3,46 +3,19 @@ import PropTypes from "prop-types";
 import { Modal } from "antd";
 import { connectModal } from "redux-modal";
 import './styles.css';
-import axios from 'axios';
 
 class ModalDownload extends Component {
-  static propTypes = {
+   static propTypes = {
     handleHide: PropTypes.func.isRequired
   };
 
-  state = {
-    posts: [ ]
-  }
 
-
-   
-
-componentDidMount() {
   
-   axios.get('https://jsonplaceholder.typicode.com/posts')
-                .then(res => {
-                console.log(res);
-             this.setState({
-            posts: res.data.slice(0, 10)
-            })
-          })
-  }
 
   render() {
-
+    console.log(this.props);
     const { show, handleHide } = this.props;
-    const { posts } = this.state; 
-     const postList = posts.length ? 
-     (posts.map(post => {
-         return (
-           <div className="post-card" key={post.id}>
-              <div className="card-content">{post.body}</div>
-           </div>
-         )})) 
-         
-         : 
-
-         (<div className="center">No posts yet</div>) 
+    
 
 
 
@@ -54,24 +27,15 @@ componentDidMount() {
         onOk={handleHide}
         onCancel={handleHide}
         footer={null}
-      >
-   
+        data={this.props}
+      >  
+        <br></br>
+        Put Axios and<br></br> Firebase code here.
+        {/* <p>{postList}</p> */}
+        <h1>{this.props.kLkd}</h1>
+        <h2>{this.props.children}</h2>
+        <br></br>
 
-
-
-
-          <br></br>
-          Put Axios and<br></br> Firebase code here.
-
-          <p>{postList}</p>
-
-
-          <br></br>
-
-
-
-
-      
       </Modal>
     );
   }
