8bca81c63f43a24dd0a57cfc9babab794e879d00
dchavours <30337368+dchavours@users.noreply.github.com>
2019-10-02 21:22:04 -0400

    Added a few files - firebase.
    Added the files running into the problem of firebase doesn't allow duplicate apps. Think I need to change how the app queues firebase cloud messaging.

--- src/index.js
+++ src/index.js
@@ -5,8 +5,13 @@ import 'materialize-css/dist/js/materialize.min';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { initializeFirebase } from './components/push-notification';
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
