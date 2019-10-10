36319557810a3bfc4a13f739f6fe0bd5840a858b
dchavours <dchavours@gmail.com>
2019-10-09 21:47:33 -0500

    no duplicate initializeApp's

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -6,10 +6,27 @@ importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
 // This mofo. 
 
 
+// This line is in initalize.js
+// const messaging = firebase.messaging();
+
+import messaging from '../src/components/PushNotifications/initialize';
 
 
-firebase.initializeApp({
-  messagingSenderId: 'your messagingSenderId', // troque pelo seu sender id
+
+messaging.setBackgroundMessageHandler(function (payload) {
+  console.log('We Received a background message ', payload);
+
+  const notificationTitle = 'New Background Message Title';
+  const notificationOptions = {
+      body: 'Fallback Message body.',
+      icon: 'logo.png'
+  };
+
+  return self.registration.showNotification(notificationTitle,
+      notificationOptions);
 });
 
-const messaging = firebase.messaging();
+self.addEventListener("notificationclick", function (event) {
+  const clickedNotification = event.notification;
+  clickedNotification.close();
+ })
