78b18ae3e63e72a7be4921c844d39c47c25bf0a4
dchavours <dchavours@gmail.com>
2019-10-15 02:13:27 -0500

    commiting deleted firebase file. Just need to create a genetor function with firebase logic.

--- src/components/PushNotifications/initialize.js
+++ src/components/PushNotifications/initialize.js
@@ -27,3 +27,17 @@ export function initializePush() {
     
 }
 
+
+
+
+
+
+
+// I'm going to have to write this so that when I'm in firebase console and I add a new entry in the Realtime Database it is sent as a push notification through the browser.
+// I'm going to do this by making an on update function which in the function is an async function that is going to have 'fetch' protocal. 
+// Moreover, an update function is ran and then a fetch is ran as well. 
+// Moreover, an update function is ran and then a fetch is ran as well. 
+
+
+
+
