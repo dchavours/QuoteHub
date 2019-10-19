907bccdc713889f0929266beee0b1a70df90ed36
dchavours <dchavours@gmail.com>
2019-10-19 00:38:55 -0500

    msg

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -6,7 +6,6 @@ firebase.initializeApp(FirebaseConfig);
 // Firebase messaging
 export const messaging = firebase.messaging();
 
-//
 
 const databaseRef = firebase.database().ref();
 export const todoRef = databaseRef.child('messages');
@@ -14,13 +13,3 @@ export const tokenRef = todoRef.child("userTokens");
 
 
 
-
-// import * as firebase from 'firebase';
-// import FirebaseConfig from './dev';
-
-// firebase.initializeApp(FirebaseConfig);
-
-// const databaseRef = firebase.database().ref();
-// const todoRef = databaseRef.child('messages');
-
-// export default todoRef;
