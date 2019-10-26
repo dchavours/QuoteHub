886462dd4bd95fda1422c19fc81a5204fa194829
dchavours <dchavours@gmail.com>
2019-10-26 03:37:47 -0500

    I think I did it.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -29,7 +29,7 @@ class LoginPage extends Component {
     const { isAuthenticated, error } = this.props;
 
     if (isAuthenticated) {
-      return <Redirect to="/" />;
+      return <Redirect to="/dashboard" />;
     }
 
     return (
--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -1,8 +1,20 @@
-import { all } from 'redux-saga/effects';
+import { all, fork } from 'redux-saga/effects';
 import todoSagas from './todo/saga';
 import modalSagas from './modal/saga';
 import tokenSagas from './pushNotifications/saga'
+import loginSaga from './auth/saga'
+
+
+// export default function* rootSaga() {
+//   yield all([todoSagas(), modalSagas(), tokenSagas()]);
+// }
+
 
 export default function* rootSaga() {
-  yield all([todoSagas(), modalSagas(), tokenSagas()]);
+  // Apparently login saga cannot be made all.
+
+  yield fork(todoSagas);
+  yield fork(modalSagas);
+  yield fork(tokenSagas);
+  yield fork(loginSaga);
 }
