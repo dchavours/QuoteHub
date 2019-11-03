39e215bdee75f469d3bdd891cadefda4a7255725
dchavours <dchavours@gmail.com>
2019-11-03 07:15:08 -0600

    Got the page to work, just need to authenticate the full routes and connect to API's. GraphQL, Rest etc.

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -5,12 +5,17 @@ import  LoginPage  from "../components/Login/Login";
 
 import DashboardPage from '../containers/Dashboard';
 
+import Charts from '../components/Chart/Charts';
+
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact render={() => <Redirect to="/dashboard" />} />
+        {/* Must be authenticated */}
         <Route path="/dashboard" exact component={DashboardPage} />
+        <Route path="/charts" exact component={Charts} />
+          {/* Must be authenticated */}
         <Route path="/Login" exact component={LoginPage} />
         <Route path="/html/quotehub.html" exact render={() => <Redirect to="/login" />} />
       </Switch>
